1a998e34d26ad6b1fcde8c1fbc1933370857de4d
mendarrr <sarahabby541@gmail.com>
2024-04-12 00:53:28 +0300

    Code update

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-// Events that occur when the page loads
-let booksVisible = false; // Initialize booksVisible variable
-let archiveVisible = false; // Initialize archiveVisible variable
+// Global Variables declared
+let booksVisible = false; 
+let archiveVisible = false;
 accessBooks();
 
-// Function that allows user to access all book titles and their authors in a list
+// Function that allows user to access all book titles in the list
 function accessBooks() {
   const showBooksButton = document.querySelector("#showBooks");
   const booklist = document.querySelector("#booklist");
+// Card that displays the book's details
   const bookDetailsCard = document.querySelector("#bookDetailsCard");
+// Declare a variable for books removed from the current reading list
   const archivedBooks = document.querySelector("#readBooks");
 
   showBooksButton.addEventListener("click", () => {
@@ -23,36 +25,41 @@ function accessBooks() {
             const li = document.createElement("li");
             li.textContent = `${book.title}`;
 
-            // Add an archive button
+            // Add an archive button which handles the achievedBooks function
             const archiveBtn = document.createElement("button");
-            archiveBtn.classList.add('btn', 'btn-archive'); // Add custom classes for styling
-            archiveBtn.innerHTML = '<i class="fa-solid fa-box-archive fa-beat"></i>'; // Use Font Awesome bookmark icon
+            // Add a class list for easy styling in CSS 
+            archiveBtn.classList.add('btn', 'btn-archive');
+            // Use an icon as a button instead of text-content
+            archiveBtn.innerHTML = '<i class="fa-solid fa-box-archive fa-beat"></i>';
             archiveBtn.addEventListener("click", () => archivedBooks(book.id));
             li.appendChild(archiveBtn);
 
-            // Add an event listener to the li
+            // Add an event listener to the li element created
             li.addEventListener("click", () => showBookDetails(book.id));
             booklist.appendChild(li);
           });
-          booksVisible = true; // Update booksVisible to true after showing the list
+          // Update booksVisible to true after showing the list
+          booksVisible = true; 
         })
         .catch((error) => console.error("Error: " + error));
     } else {
       // If books are already visible, clear the list
       booklist.innerHTML = "";
-      booksVisible = false; // Update booksVisible to false
+      // Update booksVisible to false
+      booksVisible = false; 
     }
   });
 }
 
-// Function to show book details
+// Function to show book details on the card
 function showBookDetails(id) {
-  // Fetch book details and display them
   fetch(`http://localhost:3000/books/${id}`)
     .then((response) => response.json())
     .then((book) => {
       bookDetailsCard.innerHTML = "";
-      bookDetailsCard.innerHTML = `<h2>${book.title}</h2>
+      bookDetailsCard.innerHTML = 
+      // Display the book details by using the key values of the book object as in the db.json file
+        `<h2>${book.title}</h2>
         <p>Authors: ${book.authors}</p>
         <p>Edition: ${book.edition}</p>
         <p>Publication Date: ${book.publication_date}</p>
@@ -64,8 +71,8 @@ function showBookDetails(id) {
     .catch((error) => console.error("Error: " + error));
 }
 
-accessArchive();
 // Function that allows the user to access the archive
+accessArchive();
 function accessArchive() {
   const showArchiveBtn = document.querySelector("#readBooks");
   const archivelist = document.querySelector("#archivelist");
@@ -82,10 +89,13 @@ function accessArchive() {
             const li = document.createElement("li");
             li.textContent = `${book.title}`;
 
-            // Add a delete button
+            // Add a delete button for each book listed
             const deleteBtn = document.createElement("button");
-            deleteBtn.classList.add("btn", "btn-danger"); // Add custom classes for styling
+            // Add a class list for easy styling in CSS
+            deleteBtn.classList.add("btn", "btn-danger");
+            // Use an icon as a button
             deleteBtn.innerHTML = '<i class="fa-solid fa-trash fa-fade"></i>';
+            // Add an event listener to the delete button
             deleteBtn.addEventListener("click", () => deleteBook(book.id));
             li.appendChild(deleteBtn);
             // Add an event listener to each delete button
@@ -93,17 +103,19 @@ function accessArchive() {
               const bookId = event.target.dataset.bookId;
               deleteBook(bookId);
             });
-            // Add an event listener to the li
+            // Add an event listener to the li element
             li.addEventListener("click", () => showArchiveDetails(book.id));
             archivelist.appendChild(li);
           });
-          archiveVisible = true; // Update archiveVisible to true after showing the list
+          // Update archiveVisible to true after showing the list
+          archiveVisible = true; 
         })
         .catch((error) => console.error("Error: " + error));
     } else {
       // If archive is already visible, clear the list
       archivelist.innerHTML = "";
-      archiveVisible = false; // Update archiveVisible to false
+      // Update archiveVisible to false
+      archiveVisible = false; 
     }
   });
 }
@@ -116,7 +128,7 @@ function deleteBook(bookId) {
   })
     .then((response) => {
       if (response.ok) {
-        // If the deletion is successful, remove the corresponding list item from the DOM
+        // If the deletion is successful, remove the corresponding list item
         const listItemToRemove = document.querySelector(
           `li[data-book-id="${bookId}"]`
         );
@@ -129,14 +141,15 @@ function deleteBook(bookId) {
     })
     .catch((error) => console.error("Error: " + error));
 }
-// Function to show archive details
+// Function to show archive details on the card
 function showArchiveDetails(id) {
-  // Fetch archive details and display them
   fetch(`http://localhost:3000/archive/${id}`)
     .then((response) => response.json())
     .then((book) => {
       archiveDetailsCard.innerHTML = "";
-      archiveDetailsCard.innerHTML = `<h2>${book.title}</h2>
+      archiveDetailsCard.innerHTML = 
+      // Display the book details by using the key values of the book object as in the db.json file
+    `<h2>${book.title}</h2>
      <p>Authors: ${book.authors}</p>
      <p>Edition: ${book.edition}</p>
      <p>Publication Date: ${book.publication_date}</p>
@@ -147,9 +160,11 @@ function showArchiveDetails(id) {
     })
     .catch((error) => console.error("Error: " + error));
 }
-// Function that adds more books to the current list
-function formSubmission(e) {
-  e.preventDefault();
+// Function that adds more books to the Current Reading list using the form
+function formSubmission(event) {
+  // Prevent the page from refreshing when the user clicks the button
+  event.preventDefault();
+  // Create a unique id for each book for identification purposes
   const id = new Date().getTime();
   const title = document.getElementById("title").value;
   const authors = document.getElementById("authors").value;
@@ -165,6 +180,7 @@ function formSubmission(e) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
+  // Book Details using properties
       id: id,
       title: title,
       authors: authors,
@@ -181,4 +197,5 @@ function formSubmission(e) {
     })
     .catch((error) => console.error("Error: " + error));
 }
+// Add an event listener to the button
 document.querySelector("#addbks").addEventListener("submit", formSubmission);
